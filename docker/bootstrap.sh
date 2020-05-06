e40ba6fbf0528b73778e93df1b5a8514fda2f8f0
Rudi Floren <rudi.floren@gmail.com>
2020-05-06 20:45:29 +0200

    Update bootstrap.sh

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -27,6 +27,10 @@ else
         # Dendrite uses master as its main branch. If the branch is master, we probably want sytest develop
         branch_name="develop"
     fi
+    if [ "$SYTEST_TARGET" == "conduit" ]; then
+        # Dendrite uses master as its main branch. If the branch is master, we probably want sytest develop
+        branch_name="develop"
+    fi
 
     # Try and fetch the branch
     wget -q https://github.com/matrix-org/sytest/archive/$branch_name.tar.gz -O sytest.tar.gz || {
